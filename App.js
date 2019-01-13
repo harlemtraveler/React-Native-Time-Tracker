996ae106b861babf1a7d5819fd4c7482d4806f82
Tim <tjk315@gmail.com>
2019-01-12 23:25:33 -0500

    Add hardcoded "EditableTimer"s as state for App.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import uuidv4 from 'uuid/v4';
 import {
   StyleSheet,
   ScrollView,
@@ -10,7 +11,28 @@ import EditableTimer from './components/EditableTimer';
 import ToggleableTimerForm from './components/ToggleableTimerForm';
 
 export default class App extends Component {
+  // Babel plugin "transform-class-properties" allows state declared without constructor
+  state = {
+    timers: [
+      {
+        title: 'Mow the lawn',
+        project: 'House Chores',
+        id: uuidv4(),
+        elapsed: 5456099,
+        isRunning: true,
+      },
+      {
+        title: 'Bake squash',
+        project: 'Kitchen Chores',
+        id: uuidv4(),
+        elapsed: 1273998,
+        isRunning: false,
+      },
+    ],
+  };
+
   render() {
+    const { timers } = this.state;
     return (
       <View style={styles.appContainer}>
         <View style={styles.titleContainer}>
@@ -19,20 +41,17 @@ export default class App extends Component {
         <ScrollView style={styles.timerList}>
           <ToggleableTimerForm isOpen={false}/>
 
-          <EditableTimer
-            id="1"
-            title="Moew the lawn"
-            project="House Chores"
-            elapsed="8986300"
-            isRunning
-          />
-          <EditableTimer
-            id="2"
-            title="Bake squash"
-            project="Kitchen Chores"
-            elapsed="3890985"
-            editFormOpen
-          />
+          {timers.map(({ title, project, id, elapsed, isRunning }) => (
+            <EditableTimer
+              key={id} // "key" is used by React Native for identification
+              id={id}
+              title={title}
+              project={project}
+              elapsed={elapsed}
+              isRunning={isRunning}
+            />
+          ))}
+
         </ScrollView>
       </View>
     );
